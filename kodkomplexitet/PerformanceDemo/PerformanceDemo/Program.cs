dbaf0643a2eeadb8e1ef8bfb3d310925e7275a31
Erik Stenbäcka <erik.stenbacka@sigma.se>
2020-12-15 13:35:30 +0100

    Update big-o project code to better show the difference between the implementations

--- kodkomplexitet/PerformanceDemo/PerformanceDemo/Program.cs
+++ kodkomplexitet/PerformanceDemo/PerformanceDemo/Program.cs
@@ -9,46 +9,82 @@ namespace PerformanceDemo
 {
     class Program
     {
-        static void PerformActionSimple(int count)
+        static void ConstansTimeExample(List<String> list)
         {
-            var result = "";
-            for (var i = 0; i < count; i++)
-            {
-                Thread.Sleep(10);
-                result += i;
-            }
-            Console.WriteLine($"Result is {result.Length}");
+            Console.WriteLine($"The length of the list is {list.Count}");
+            Thread.Sleep(10); // included to make time measurement easier
         }
 
-        static void PerformActionComplex(int count)
+        static void LinearTimeExample(List<String> list)
         {
-            var result = "";
-            for (var i = 0; i < count; i++)
+            foreach (var item in list)
             {
-                for (var j = 0; j < count; j++)
+                Console.WriteLine($"Item value: {item}");
+                Thread.Sleep(10); // included to make time measurement easier
+            }
+        }
+
+        static String LogarithmicTimeExample(List<String> list)
+        {
+            if (list.Count == 1)
+            {
+                Thread.Sleep(10); // included to make time measurement easier
+                return list[0];
+            }
+            else
+            {
+                Thread.Sleep(10); // included to make time measurement easier
+                return LogarithmicTimeExample(list.GetRange(0, (int)Math.Ceiling(list.Count / 2.0)));
+            }
+        }
+
+        static void QuadraticTimeExample(List<String> list)
+        {
+            foreach (var item in list)
+            {
+                foreach (var otherItem in list)
                 {
-                    Thread.Sleep(10);
-                    result += i;
+                    Console.WriteLine($"Some computation on {item} and {otherItem}");
+                    Thread.Sleep(10); // included to make time measurement easier
                 }
             }
-            Console.WriteLine($"Result is {result.Length}");
         }
+
 
         static void Main(string[] args)
         {
             var stopWatch = new Stopwatch();
+            var list = new List<string> { "item-1", "item-2", "item-3", "item-1", "item-2", "item-3", "item-1", "item-2", "item-3" };
 
             stopWatch.Start();
-            PerformActionSimple(100);
+            ConstansTimeExample(list);
             stopWatch.Stop();
-            Console.WriteLine($"It took: {stopWatch.ElapsedMilliseconds} ms");
+            var constantTime = stopWatch.ElapsedMilliseconds;
+            stopWatch.Reset();
 
-            var stopWatch2 = new Stopwatch();
+            stopWatch.Start();
+            LogarithmicTimeExample(list);
+            stopWatch.Stop();
+            var logTime = stopWatch.ElapsedMilliseconds;
+            stopWatch.Reset();
 
-            stopWatch2.Start();
-            PerformActionComplex(100);
-            stopWatch2.Stop();
-            Console.WriteLine($"It took: {stopWatch2.ElapsedMilliseconds} ms");
+            stopWatch.Start();
+            LinearTimeExample(list);
+            stopWatch.Stop();
+            var linearTime = stopWatch.ElapsedMilliseconds;
+            stopWatch.Reset();
+
+            stopWatch.Start();
+            QuadraticTimeExample(list);
+            stopWatch.Stop();
+            var quaricTime = stopWatch.ElapsedMilliseconds;
+            stopWatch.Reset();
+
+            Console.WriteLine();
+            Console.WriteLine($"Constant time: {constantTime} ms");
+            Console.WriteLine($"Logaritmic time: {logTime} ms");
+            Console.WriteLine($"Linear time: {linearTime} ms");
+            Console.WriteLine($"Quadratic time: {quaricTime} ms");
 
             Console.Read();
         }
